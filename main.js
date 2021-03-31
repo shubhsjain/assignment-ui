d88a84baea5310aedda7a278213696eae4ff4223
shubhsjain <svj8866@gmail.com>
2021-03-31 18:40:13 +0530

    complete single page ui not responsive

---
+++ main.js
@@ -0,0 +1,155 @@
+function getAll() {
+  var c = document.getElementById("vscard")
+  var b = document.getElementById("recard2")
+  var a = document.getElementById("recard")
+  var z = document.getElementById("webcard2");
+  var x = document.getElementById("allcard")
+  var y = document.getElementById("webcard")
+  if (x.style.display === "none") {
+    c.style.display ="none"
+    b.style.display = "none"
+    a.style.display = "none"
+    z.style.display = "none";
+    x.style.display= "block"
+    y.style.display="none"
+  } else {
+    c.style.display ="none"
+    b.style.display = "none"
+   a.style.display = "none"
+   z.style.display= "none"
+   x.style.display="block"
+   y.style.display="none"
+  }
+  }
+
+      // WEB HOSTING
+
+  function getWeb() {
+    var c = document.getElementById("vscard")
+    var b = document.getElementById("recard2")
+    var a = document.getElementById("recard")
+    var z = document.getElementById("webcard2");
+    var x = document.getElementById("allcard")
+    var y = document.getElementById("webcard")
+    if (y.style.display === "none") {
+      c.style.display ="none"
+      b.style.display = "none"
+      a.style.display = "none"
+      z.style.display = "none";
+      x.style.display= "none"
+      y.style.display="block"
+    } else {
+      c.style.display ="none"
+      b.style.display = "none"
+     a.style.display = "none"
+     z.style.display= "none"
+     x.style.display="none"
+     y.style.display="block"
+    }
+  }
+
+
+    // WEB BUTTON
+
+  function next() {
+    var c = document.getElementById("vscard")
+    var b = document.getElementById("recard2")
+    var a = document.getElementById("recard")
+    var z = document.getElementById("webcard2");
+    var x = document.getElementById("allcard")
+    var y = document.getElementById("webcard")
+    if (z.style.display === "none") {
+      c.style.display ="none"
+      b.style.display = "none"
+      a.style.display = "none"
+      z.style.display = "block";
+      x.style.display= "none"
+      y.style.display="none"
+    } else {
+      c.style.display ="none"
+      b.style.display = "none"
+     a.style.display = "none"
+     z.style.display= "block"
+     x.style.display="none"
+     y.style.display="none"
+    }
+  }
+
+      // RESELLER HOSTING
+  function getRe() {
+    var c = document.getElementById("vscard")
+    var b = document.getElementById("recard2")
+    var a = document.getElementById("recard")
+    var z = document.getElementById("webcard2");
+    var x = document.getElementById("allcard")
+    var y = document.getElementById("webcard")
+    if (a.style.display === "none") {
+      c.style.display ="none"
+      b.style.display = "none"
+      a.style.display = "block"
+      z.style.display = "none";
+      x.style.display= "none"
+      y.style.display="none"
+    } else {
+      c.style.display ="none"
+      b.style.display = "none"
+     a.style.display = "block"
+     z.style.display= "none"
+     x.style.display="none"
+     y.style.display="none"
+    }
+  }
+
+      // RESELLER NEXT BUTTON
+  function next2() {
+    var c = document.getElementById("vscard")
+    var b = document.getElementById("recard2")
+    var a = document.getElementById("recard")
+    var z = document.getElementById("webcard2");
+    var x = document.getElementById("allcard")
+    var y = document.getElementById("webcard")
+    if (b.style.display === "none") {
+      c.style.display ="none"
+      b.style.display = "block"
+      a.style.display = "none"
+      z.style.display = "none";
+      x.style.display= "none"
+      y.style.display="none"
+    } else {
+      c.style.display ="none"
+      b.style.display = "block"
+     a.style.display = "none"
+     z.style.display= "none"
+     x.style.display="none"
+     y.style.display="none"
+    }
+  }
+
+    // VPS HOSTING
+
+    function getVPS() {
+      var c = document.getElementById("vscard")
+      var b = document.getElementById("recard2")
+      var a = document.getElementById("recard")
+      var z = document.getElementById("webcard2");
+      var x = document.getElementById("allcard")
+      var y = document.getElementById("webcard")
+      if (c.style.display === "none") {
+        c.style.display ="block"
+        b.style.display = "none"
+        a.style.display = "none"
+        z.style.display = "none";
+        x.style.display= "none"
+        y.style.display="none"
+      } else {
+        c.style.display ="block"
+        b.style.display = "none"
+       a.style.display = "none"
+       z.style.display= "none"
+       x.style.display="none"
+       y.style.display="none"
+      }
+    }
+
+
+  
